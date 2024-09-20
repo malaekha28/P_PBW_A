274b4b8e632186bf9c9920b9151a8294e2e3330f
MalaekhaIskandar <145976346+malaekha28@users.noreply.github.com>
2024-09-20 10:55:01 +0700

    Add files via upload

---
+++ Pertemuan 3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page - Buku Tamu</title>
+</head>
+<body>
+    <h1>Selamat Datang di Buku Tamu</h1>
+    <p>Selamat datang di situs buku tamu kami. Silakan mengisi buku tamu atau melihat daftar tamu yang telah mengisi.</p>
+
+    <ul>
+        <li><a href="input.php">Isi Buku Tamu</a></li>
+        <li><a href="tabel.php">Lihat Daftar Tamu</a></li>
+    </ul>
+</body>
+</html>
